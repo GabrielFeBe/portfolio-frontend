f8e32b5e5635926985e01f827bfbc80a9064e9d4
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-07-26 23:04:29 -0300

    começando formulario

---
+++ src/app/components/StatementBall.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+  orange: string
+  rest: string
+  bgColor: string
+}
+
+export default function StatementBall({ orange, rest, bgColor }: Props) {
+  return (
+    <h2
+      className={`flex h-[60px] w-full items-center ${bgColor} justify-center  text-5xl`}
+    >
+      <div className="flex h-[55px] w-[55px] flex-shrink-0 items-center justify-end rounded-full bg-gradient-to-r from-customOrange to-customRed">
+        {orange}
+      </div>
+      {rest}
+    </h2>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { api } from '@/lib/api'
 import Project from '@/types/Projects'
 import Stacks from './components/Stacks'
 import ProjectsHome from './components/ProjectsHome'
+import StatementBall from './components/StatementBall'
 
 export default async function Home() {
   const response = await api.get('/favorites')
@@ -21,13 +22,31 @@ export default async function Home() {
         <Stacks />
       </div>
       {/* down side */}
-      <h2 className="flex h-[60px] w-full items-center justify-center bg-customSlate text-5xl">
-        <div className="flex h-[55px] w-[55px] flex-shrink-0 items-center justify-end rounded-full bg-gradient-to-r from-customOrange to-customRed">
-          P
-        </div>
-        rojetos
-      </h2>
+      <StatementBall orange="P" rest="rojetos" bgColor="bg-customSlate" />
       <ProjectsHome projects={data} />
+
+      {/* Footer */}
+      <footer className="min-h-4/6-screen bg-customGray">
+        <StatementBall orange="C" rest="ontate-me" bgColor="bg-customGray" />
+        <div className="grid min-h-4/6-screen grid-cols-2">
+          <div className="mt-3 pl-20">
+            <input
+              type="text"
+              className="border-gradient border bg-customGray"
+              placeholder="Nome"
+            />
+            <input
+              type="text"
+              className="border-gradient border bg-customGray"
+              placeholder="Sobrenome"
+            />
+            <input
+              type="email"
+              className="border-gradient border bg-customGray"
+            />
+          </div>
+        </div>
+      </footer>
     </main>
   )
 }
