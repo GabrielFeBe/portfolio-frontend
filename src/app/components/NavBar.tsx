b1bee05491ffcc03a2bdf0915a472b8e43c23bec
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-10-18 19:34:52 -0300

    melhorando nav-bar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -11,6 +11,18 @@ export default function NavBar() {
     <CloseNavBar>
       <div className=" flex h-full  flex-col items-center justify-between sm:w-[650px] sm:flex-row lg:w-[800px]">
         <Link
+          href="/"
+          className="hover:mix-blend inline-block self-center rounded-full bg-gradient-to-r from-customOrange to-customRed px-5 py-3 text-center text-sm leading-none text-black transition duration-300 hover:opacity-70 sm:w-44"
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:mix-blend inline-block self-center rounded-full bg-gradient-to-r from-customOrange to-customRed px-5 py-3 text-center text-sm leading-none text-black transition duration-300 hover:opacity-70 sm:w-44"
+          href="/projects"
+        >
+          Projetos
+        </Link>
+        <Link
           href="/adding"
           className="hover:mix-blend inline-block self-center rounded-full bg-gradient-to-r from-customOrange to-customRed px-5 py-3 text-center text-sm leading-none text-black transition duration-300 hover:opacity-70 sm:w-44"
         >
@@ -26,18 +38,6 @@ export default function NavBar() {
         ) : (
           <Sair></Sair>
         )}
-        <Link
-          href="/"
-          className="hover:mix-blend inline-block self-center rounded-full bg-gradient-to-r from-customOrange to-customRed px-5 py-3 text-center text-sm leading-none text-black transition duration-300 hover:opacity-70 sm:w-44"
-        >
-          Home
-        </Link>
-        <Link
-          className="hover:mix-blend inline-block self-center rounded-full bg-gradient-to-r from-customOrange to-customRed px-5 py-3 text-center text-sm leading-none text-black transition duration-300 hover:opacity-70 sm:w-44"
-          href="/projects"
-        >
-          Projetos
-        </Link>
       </div>
     </CloseNavBar>
   )
