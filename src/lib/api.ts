b7f0ce5f91660df0acaa85c8b8cafd496ae38973
Gabriel Fernandes <hu3master.zord@hotmail.com>
2024-02-02 22:11:37 -0300

    trocando apis endpoints

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,11 +3,9 @@ import axios from 'axios'
 // Verifica se estamos em um ambiente de desenvolvimento local
 const isLocal = process.env.NODE_ENV === 'development'
 
-// Obtém a porta da variável de ambiente PORT ou define como padrão 3001 para desenvolvimento local
-const port = isLocal ? process.env.PORT || 3000 : ''
-
-// Constrói a baseURL com a porta, se estiver disponível
-const baseURL = isLocal ? `http://localhost:${port}/api` : '/api'
+const baseURL = isLocal
+  ? `http://localhost:3000/api`
+  : 'https://gabrielfedev.vercel.app/api'
 
 export const api = axios.create({
   baseURL,
